1ebc5b532b4f58a653889313c1db1a4b59fe7d4f
olyaturova <olyaturova84@gmail.com>
2026-01-13 19:20:52 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+     open: true,
+     port: 3000
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'), 
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'] 
   },
-  server: {
-    historyApiFallback: true,
-  },
-  build: {
-    outDir: 'dist',
-  }
-})
+});
